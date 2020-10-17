ccf6c5f89e303937be06b2372aafd711d03143f0
arin zbook <arin_muk@hotmail.com>
2020-10-17 12:20:46 -0400

    alternate

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask,render_template,jsonify,request
 import json
 from pymongo import MongoClient 
 from connections import cloudM_R
+import os 
+
+port = int(os.environ.get('PORT', 5000)) 
+#app.run(host='0.0.0.0', port=port)
 
 
 app=Flask(__name__)
@@ -33,4 +37,4 @@ def read():
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
